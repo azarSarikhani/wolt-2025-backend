e34ece972ee41adc7bf890c5e88d26f0168b996c
Sara <sara.hanifpour@sok.fi>
2025-01-18 13:36:54 +0200

    distance

--- lib/src/dopc/tools/priceCalculator.py
+++ lib/src/dopc/tools/priceCalculator.py
@@ -1,8 +1,34 @@
 import sys
+import numpy as np
+import geopy.distance
+from dopc.tools.Venue import Venue
 
 
-def __call__(inputs: dict) -> dict[str, float]:
-    # toeo
+
+query_inputs = {'venue_slug': 'home-assignment-venue-helsinki', 'cart_value': 1000, 'user_lat':60.17094, 'user_lon': 24.93087}
+
+
+
+
+def geoDistance(coord1: tuple, coords2: tuple) -> float:
+    distance = geopy.distance.geodesic(coords_1, coords_2).m
+    #d = norm(np.cross(p2-p1, p1-p3))/norm(p2-p1)
+    return int(np.round(distance))
+
+def __call__(query_inputs: dict,
+            static_info: dict,
+            dynamic_info: dict) -> dict[str, float]:
+    venue = Venue(venue_slug=query_inputs.get('venue_slug'))
+    response = venue.getDynamicIfo()
+    dynamicInfo= venue.parseVenueDynamicInfo(response)
+
+    response = venue.getStaticicIfo()
+    staticInfo= venue.parseVenueStaticInfo(response)
+    x1 = query_inputs.get('user_lat')
+    y1 = query_inputs.get('user_lon')
+    x2 = staticInfo.get('COORDINATES')[1]
+    y2 =  staticInfo.get('COORDINATES')[0]
+    distance = geoDistance((x1, y1), (x2, y2))
     return {'price': 10}
 
 
